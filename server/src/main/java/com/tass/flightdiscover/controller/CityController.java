648810994c204e2b2a016e2e4c304ea47aee951f
Eryk <emroczko@me.com>
2023-01-08 11:52:28 +0100

    Removed not necessary methods

--- server/src/main/java/com/tass/flightdiscover/controller/CityController.java
+++ server/src/main/java/com/tass/flightdiscover/controller/CityController.java
@@ -1,12 +1,15 @@
 package com.tass.flightdiscover.controller;
 
+import com.tass.flightdiscover.domain.city.CityRequest;
 import com.tass.flightdiscover.domain.city.CityResponse;
-import com.tass.flightdiscover.exceptions.CityNotFoundException;
 import com.tass.flightdiscover.service.CityService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -20,18 +23,8 @@ public class CityController {
     private final CityService cityService;
 
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
-    public ResponseEntity<List<CityResponse>> getAllCities() {
+    public ResponseEntity<List<CityResponse>> getAllCities(CityRequest request) {
+        log.info("City request: {}", request);
         return ResponseEntity.ok(this.cityService.getCities());
-    }
-
-    @RequestMapping(value = "/getByName", method = RequestMethod.GET)
-    public ResponseEntity<CityResponse> getCity(String name) throws CityNotFoundException {
-        log.info("Searching for city: {}", name);
-        return ResponseEntity.ok(cityService.getCity(name));
-    }
-
-    @GetMapping(value = "/getPopularByRatio")
-    public ResponseEntity<List<CityResponse>> getPopularCities(@RequestParam Double populationToFlightsRatio) {
-        return ResponseEntity.ok(cityService.getPopularCities(populationToFlightsRatio));
     }
 }
