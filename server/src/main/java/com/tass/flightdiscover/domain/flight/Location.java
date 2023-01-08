5f629d07e8f2785fd05355adbea747b091a07bc7
Eryk <emroczko@me.com>
2023-01-08 11:54:09 +0100

    Added validation to Location class

--- server/src/main/java/com/tass/flightdiscover/domain/flight/Location.java
+++ server/src/main/java/com/tass/flightdiscover/domain/flight/Location.java
@@ -1,5 +1,6 @@
 package com.tass.flightdiscover.domain.flight;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Location {
+    @NotBlank(message = "City cannot be null")
     private String city;
+    @NotBlank(message = "Country cannot be null")
     private String country;
 }
