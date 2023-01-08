d4f9756fca5094f3d93dc143624bb0d47623b2df
Eryk <emroczko@me.com>
2023-01-08 11:53:36 +0100

    Added finding connections methods

--- server/src/main/java/com/tass/flightdiscover/controller/FlightsController.java
+++ server/src/main/java/com/tass/flightdiscover/controller/FlightsController.java
@@ -1,15 +1,18 @@
 package com.tass.flightdiscover.controller;
 
-import com.tass.flightdiscover.FlightsFacade;
+import com.tass.flightdiscover.domain.flight.ConnectionRequest;
 import com.tass.flightdiscover.domain.flight.FlightRequest;
 import com.tass.flightdiscover.domain.flight.FlightResponse;
-import com.tass.flightdiscover.exceptions.CityNotFoundException;
+import com.tass.flightdiscover.exceptions.FlightNotFoundException;
+import com.tass.flightdiscover.facades.FlightsFacade;
+import com.tass.flightdiscover.validators.RequestValidator;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,9 +26,15 @@ public class FlightsController {
 
     private final FlightsFacade flightsFacade;
 
-    @GetMapping(value = "/getByLocations")
-    public ResponseEntity<List<FlightResponse>> findFlight(FlightRequest request) throws CityNotFoundException {
+    @RequestMapping(value = "/getByToAndFrom", method = RequestMethod.GET)
+    public ResponseEntity<List<FlightResponse>> findFlight(@Valid FlightRequest request) throws FlightNotFoundException {
         log.info("Find flight request {}", request);
         return ResponseEntity.ok(flightsFacade.getFlights(request));
+    }
+
+    @RequestMapping(value = "/getAllConnectionsBetween", method = RequestMethod.GET)
+    public ResponseEntity<List<FlightResponse>> getAllConnectionsBetween(@Valid ConnectionRequest request) throws FlightNotFoundException {
+        log.info("Check connections request {}", request);
+        return ResponseEntity.ok(flightsFacade.getAllConnections(request));
     }
 }
--- server/src/main/java/com/tass/flightdiscover/service/FlightsService.java
+++ server/src/main/java/com/tass/flightdiscover/service/FlightsService.java
@@ -1,44 +1,35 @@
 package com.tass.flightdiscover.service;
 
-import com.tass.flightdiscover.converters.FlightConversionService;
-import com.tass.flightdiscover.domain.flight.Flight;
+import com.tass.flightdiscover.converters.FlightConverter;
 import com.tass.flightdiscover.domain.flight.FlightResponse;
 import com.tass.flightdiscover.domain.flight.Location;
-import com.tass.flightdiscover.exceptions.CityNotFoundException;
 import com.tass.flightdiscover.repository.FlightsRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class FlightsService {
     private final FlightsRepository flightsRepository;
-    private final FlightConversionService flightConversionService;
+    private final FlightConverter flightConverter;
 
     public List<FlightResponse> getFlightsByOriginLocation(Location origin) {
-        return flightConversionService.convertList(flightsRepository
+        return flightConverter.convertList(flightsRepository
                 .findByOriginCityAndOriginCountry(origin.getCity(), origin.getCountry()));
     }
 
     public List<FlightResponse> getFlightsByDestinationLocation(Location destination) {
-        return flightConversionService.convertList(flightsRepository
+        return flightConverter.convertList(flightsRepository
                 .findByDestinationCityAndDestinationCountry(destination.getCity(), destination.getCountry()));
     }
 
-    public FlightResponse getFlightsByOriginAndDestinationLocation(Location origin, Location destination) throws CityNotFoundException {
-        return flightsRepository.findByOriginCityAndOriginCountryAndDestinationCityAndDestinationCountry(
-                        origin.getCity(), origin.getCountry(), destination.getCity(), destination.getCountry())
-                .map(flightConversionService::convert)
-                .orElseThrow(() -> {
-                    var message = "Flight from %s, %s to %s, %s not found!".formatted(origin.getCity(), origin.getCountry(), destination.getCity(), destination.getCountry());
-                    log.info(message);
-                    return new CityNotFoundException(message);
-    });
+    public List<FlightResponse> getFlightsByOriginAndDestinationLocation(Location origin, Location destination) {
+        return flightConverter.convertList(flightsRepository.findByOriginCityAndOriginCountryAndDestinationCityAndDestinationCountry(
+                origin.getCity(), origin.getCountry(), destination.getCity(), destination.getCountry()));
     }
 
 }
