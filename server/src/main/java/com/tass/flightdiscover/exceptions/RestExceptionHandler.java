94fef8e74618ea1bd674ae46b305d1855ec33b28
Eryk <emroczko@me.com>
2023-01-08 11:52:07 +0100

    Added handling bind exception

--- server/src/main/java/com/tass/flightdiscover/exceptions/RestExceptionHandler.java
+++ server/src/main/java/com/tass/flightdiscover/exceptions/RestExceptionHandler.java
@@ -2,19 +2,33 @@ package com.tass.flightdiscover.exceptions;
 
 import com.tass.flightdiscover.domain.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestExceptionHandler {
 
     @ExceptionHandler(value = {CityNotFoundException.class, FlightNotFoundException.class})
-    protected ResponseEntity<ErrorResponse> handleConflict(Exception ex) {
+    protected ResponseEntity<ErrorResponse> handleException(Exception ex) {
         return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = BindException.class)
+    protected ResponseEntity<ErrorResponse> handleException(BindException ex, WebRequest request) {
+        var errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        log.info("Request errors: {}", errors);
+        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.NOT_FOUND);
     }
 }
