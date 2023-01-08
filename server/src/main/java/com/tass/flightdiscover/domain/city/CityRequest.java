d3cc93a4ce3bc21dd083a082964a731313a5db3d
Eryk <emroczko@me.com>
2023-01-08 11:53:54 +0100

    Create CityRequest.java

---
+++ server/src/main/java/com/tass/flightdiscover/domain/city/CityRequest.java
@@ -0,0 +1,25 @@
+package com.tass.flightdiscover.domain.city;
+
+import lombok.Data;
+
+@Data
+public class CityRequest {
+    private String name;
+    private String country;
+    private Long minPopulation;
+    private Long maxPopulation;
+    private Long minFlightsNumberFromCity;
+    private Long maxFlightsNumberFromCity;
+    private Long minFlightsNumberToCity;
+    private Long maxFlightsNumberToCity;
+    private Long minSrcCitiesNumber;
+    private Long maxSrcCitiesNumber;
+    private Long minDestCitiesNumber;
+    private Long maxDestCitiesNumber;
+    private Long minTotalConnectedCitiesNumber;
+    private Long maxTotalConnectedCitiesNumber;
+    private Long minTotalFlightsToPopulationRatio;
+    private Long maxTotalFlightsToPopulationRatio;
+    private Sort sortBy;
+    private SortOrder sortOrder;
+}
