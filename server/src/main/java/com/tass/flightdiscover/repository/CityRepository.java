19c3f473002eeb549dcd523b8f191523c7cd52e6
Eryk <emroczko@me.com>
2023-01-08 21:02:59 +0100

    Added methods to city path

--- server/src/main/java/com/tass/flightdiscover/repository/CityRepository.java
+++ server/src/main/java/com/tass/flightdiscover/repository/CityRepository.java
@@ -1,15 +1,28 @@
 package com.tass.flightdiscover.repository;
 
-import com.tass.flightdiscover.domain.city.City;
+import com.tass.flightdiscover.domain.city.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface CityRepository extends JpaRepository<City, Long> {
-    Optional<City> findCityByName(String name);
+public interface CityRepository extends JpaRepository<CityEntity, Long> {
+    List<CityEntity> findCityByName(String name);
 
-    List<City> findByTotalFlightsNumberForCityToPopulationRatioGreaterThanEqual(Double totalFlightsNumberForCityToPopulationRatiow);
+    List<CityEntity> findByTotalFlightsNumberForCityToPopulationRatioGreaterThanEqual(Double totalFlightsNumberForCityToPopulationRatio);
+//    private Long minPopulation;
+//    private Long maxPopulation;
+//    private Long minFlightsNumberFromCity;
+//    private Long maxFlightsNumberFromCity;
+//    private Long minFlightsNumberToCity;
+//    private Long maxFlightsNumberToCity;
+//    private Long minSrcCitiesNumber;
+//    private Long maxSrcCitiesNumber;
+//    private Long minDestCitiesNumber;
+//    private Long maxDestCitiesNumber;
+//    private Long minTotalConnectedCitiesNumber;
+//    private Long maxTotalConnectedCitiesNumber;
+//    private Long minTotalFlightsToPopulationRatio;
+//    private Long maxTotalFlightsToPopulationRatio;
 }
--- server/src/main/java/com/tass/flightdiscover/service/CityService.java
+++ server/src/main/java/com/tass/flightdiscover/service/CityService.java
@@ -1,13 +1,13 @@
 package com.tass.flightdiscover.service;
 
 import com.tass.flightdiscover.converters.CityConverter;
-import com.tass.flightdiscover.domain.city.CityResponse;
-import com.tass.flightdiscover.exceptions.CityNotFoundException;
+import com.tass.flightdiscover.domain.city.City;
 import com.tass.flightdiscover.repository.CityRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -18,29 +18,20 @@ public class CityService {
     private final CityRepository cityRepository;
     private final CityConverter cityConverter;
 
-    public List<CityResponse> getCities() {
-        return cityRepository
-                .findAll()
-                .stream()
-                .flatMap(city -> {
-                    var cityResponse = cityConverter.convert(city);
-                    return cityResponse != null ? Stream.of(cityResponse) : Stream.empty();
-                })
-                .toList();
+    public List<City> getAllCities() {
+        return cityConverter
+                .convertList(cityRepository.findAll());
     }
 
-    public CityResponse getCityByNameAndCountry(String name, String country) throws CityNotFoundException {
-        return cityRepository
-                .findCityByName(name)
-                .map(cityConverter::convert)
-                .orElseThrow(() -> {
-                    var message = "City %s not found!".formatted(name);
-                    log.info(message);
-                    return new CityNotFoundException(message);
-                });
+    public List<City> getCityByName(String name) {
+        return cityConverter.convertList(cityRepository.findCityByName(name));
     }
 
-    public List<CityResponse> getPopularCities(Double ratio) {
+    public List<City> filterList() {
+        return Collections.emptyList();
+    }
+
+    public List<City> getPopularCities(Double ratio) {
         return cityRepository.findByTotalFlightsNumberForCityToPopulationRatioGreaterThanEqual(ratio)
                 .stream()
                 .flatMap(city -> {
