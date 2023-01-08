a97b389c33ff255a6597c70c60cd82ca05e69cc3
Eryk <emroczko@me.com>
2023-01-08 11:53:59 +0100

    Create ConnectionRequest.java

---
+++ server/src/main/java/com/tass/flightdiscover/domain/flight/ConnectionRequest.java
@@ -0,0 +1,15 @@
+package com.tass.flightdiscover.domain.flight;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class ConnectionRequest {
+    @NotNull(message = "First location cannot be null")
+    private Location firstLocation;
+    @NotNull(message = "Second location cannot be null")
+    private Location secondLocation;
+}
