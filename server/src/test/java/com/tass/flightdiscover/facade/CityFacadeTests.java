a806930605c835eedeaab1429f4fe4706e41eb4a
Eryk <emroczko@me.com>
2023-01-15 10:42:26 +0100

    Added facade tests

--- server/src/test/java/com/tass/flightdiscover/facade/CityFacadeTests.java
+++ server/src/test/java/com/tass/flightdiscover/facade/CityFacadeTests.java
@@ -1,7 +1,112 @@
 package com.tass.flightdiscover.facade;
 
+import com.tass.flightdiscover.domain.city.CityRequest;
+import com.tass.flightdiscover.domain.city.Sort;
+import com.tass.flightdiscover.domain.city.SortOrder;
+import com.tass.flightdiscover.exceptions.CityNotFoundException;
+import com.tass.flightdiscover.facades.CityFacade;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-public class CityFacadeTests {
+class CityFacadeTests {
+
+    @Autowired
+    private CityFacade cityFacade;
+
+    @Test
+    void testGetCityByName() throws CityNotFoundException {
+        var cityName = "New York";
+        var foundCities = cityFacade.getCitiesByName(cityName);
+        var foundCity = foundCities.get(0);
+
+        assertAll(() -> {
+            assertEquals("New York", foundCity.getName());
+            assertEquals("United States", foundCity.getCountryName());
+            assertEquals(40.6943, foundCity.getCityLocationY());
+            assertEquals(-73.9249, foundCity.getCityLocationX());
+            assertEquals(19354922L, foundCity.getCityPopulation());
+            assertEquals(183503L, foundCity.getFlightsNumberToCity());
+            assertEquals(183449L, foundCity.getFlightsNumberFromCity());
+            assertEquals(86L, foundCity.getOriginCitiesNumber());
+            assertEquals(84L, foundCity.getDestinationCitiesNumber());
+            assertEquals(86L, foundCity.getTotalConnectedCitiesNumber());
+            assertEquals(2, foundCity.getOriginCountriesNumber());
+            assertEquals(2, foundCity.getDestinationCountriesNumber());
+            assertEquals(3, foundCity.getBusiestMonthFlightsToCity());
+            assertEquals(3, foundCity.getBusiestMonthFlightsFromCity());
+            assertEquals(0.009478157545662029, foundCity.getFlightsNumberFromCityToPopulationRatio());
+            assertEquals(0.009480947533655781, foundCity.getFlightsNumberToCityToPopulationRatio());
+            assertEquals(0.01895910507931781, foundCity.getTotalFlightsNumberForCityToPopulationRatio());
+        });
+    }
+
+    @Test
+    void testGetCityByNameWhenNotFound() {
+        var exception = assertThrows(CityNotFoundException.class,
+                () -> cityFacade.getCitiesByName("TESTESTEST"));
+
+        var expectedMessage = "No cities found for given parameters!";
+        var actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void testGetCities() throws CityNotFoundException {
+        var cityRequest = CityRequest.builder()
+                .minPopulation(10000000L)
+                .sortBy(Sort.POPULATION)
+                .sortOrder(SortOrder.DESC)
+                .build();
+
+        var foundCities = cityFacade.getCities(cityRequest);
+
+        assertEquals(2, foundCities.size());
+
+        var foundCity = foundCities.get(0);
+
+        assertAll(() -> {
+            assertEquals("New York", foundCity.getName());
+            assertEquals("United States", foundCity.getCountryName());
+            assertEquals(40.6943, foundCity.getCityLocationY());
+            assertEquals(-73.9249, foundCity.getCityLocationX());
+            assertEquals(19354922L, foundCity.getCityPopulation());
+            assertEquals(183503L, foundCity.getFlightsNumberToCity());
+            assertEquals(183449L, foundCity.getFlightsNumberFromCity());
+            assertEquals(86L, foundCity.getOriginCitiesNumber());
+            assertEquals(84L, foundCity.getDestinationCitiesNumber());
+            assertEquals(86L, foundCity.getTotalConnectedCitiesNumber());
+            assertEquals(2, foundCity.getOriginCountriesNumber());
+            assertEquals(2, foundCity.getDestinationCountriesNumber());
+            assertEquals(3, foundCity.getBusiestMonthFlightsToCity());
+            assertEquals(3, foundCity.getBusiestMonthFlightsFromCity());
+            assertEquals(0.009478157545662029, foundCity.getFlightsNumberFromCityToPopulationRatio());
+            assertEquals(0.009480947533655781, foundCity.getFlightsNumberToCityToPopulationRatio());
+            assertEquals(0.01895910507931781, foundCity.getTotalFlightsNumberForCityToPopulationRatio());
+        });
+
+        assertEquals("Los Angeles", foundCities.get(1).getName());
+    }
+
+    @Test
+    void testGetCitiesNotFound() {
+        var cityRequest = CityRequest.builder()
+                .minPopulation(30000000L)
+                .sortBy(Sort.POPULATION)
+                .sortOrder(SortOrder.DESC)
+                .build();
+
+        var exception = assertThrows(CityNotFoundException.class,
+                () -> cityFacade.getCities(cityRequest));
+
+        var expectedMessage = "No cities found for given parameters!";
+        var actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 }
--- server/src/test/java/com/tass/flightdiscover/facade/FlightsFacadeTests.java
+++ server/src/test/java/com/tass/flightdiscover/facade/FlightsFacadeTests.java
@@ -1,7 +1,146 @@
 package com.tass.flightdiscover.facade;
 
+import com.tass.flightdiscover.domain.flight.ConnectionRequest;
+import com.tass.flightdiscover.domain.flight.FlightRequest;
+import com.tass.flightdiscover.domain.flight.Location;
+import com.tass.flightdiscover.exceptions.BadRequestException;
+import com.tass.flightdiscover.exceptions.CityNotFoundException;
+import com.tass.flightdiscover.exceptions.FlightNotFoundException;
+import com.tass.flightdiscover.facades.FlightsFacade;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-public class FlightsFacadeTests {
+class FlightsFacadeTests {
+
+    @Autowired
+    private FlightsFacade flightsFacade;
+
+    @Test
+    void testGetFlights() throws FlightNotFoundException, BadRequestException {
+
+        var originCity = "Aberdeen";
+        var originCountry = "United States";
+        var destinationCity = "Minneapolis";
+        var destinationCountry = "United States";
+
+        var flightRequest = FlightRequest.builder()
+                .from(Location.builder().city(originCity).country(originCountry).build())
+                .to(Location.builder().city(destinationCity).country(destinationCountry).build())
+                .build();
+
+        var foundFlights = flightsFacade.getFlights(flightRequest);
+
+        assertEquals(1, foundFlights.size());
+
+        var foundFlight = foundFlights.get(0);
+        assertAll(() -> {
+            assertEquals(671, foundFlight.getNumberOfFlights());
+            assertEquals(originCity, foundFlight.getFrom().getCity());
+            assertEquals(originCountry, foundFlight.getFrom().getCountry());
+            assertEquals(destinationCity, foundFlight.getTo().getCity());
+            assertEquals(destinationCountry, foundFlight.getTo().getCountry());
+        });
+    }
+
+    @Test
+    void testGetFlightsReversed() throws FlightNotFoundException, BadRequestException {
+
+        var originCity = "Aberdeen";
+        var originCountry = "United States";
+        var destinationCity = "Minneapolis";
+        var destinationCountry = "United States";
+
+        var flightRequest = FlightRequest.builder()
+                .from(Location.builder().city(originCity).country(originCountry).build())
+                .to(Location.builder().city(destinationCity).country(destinationCountry).build())
+                .reverse(true)
+                .build();
+
+        var foundFlights = flightsFacade.getFlights(flightRequest);
+
+        assertEquals(1, foundFlights.size());
+
+        var foundFlight = foundFlights.get(0);
+        assertAll(() -> {
+            assertEquals(1340, foundFlight.getNumberOfFlights());
+            assertEquals(originCity, foundFlight.getTo().getCity());
+            assertEquals(originCountry, foundFlight.getTo().getCountry());
+            assertEquals(destinationCity, foundFlight.getFrom().getCity());
+            assertEquals(destinationCountry, foundFlight.getFrom().getCountry());
+        });
+    }
+
+    @Test
+    void testGetFlightsNotFound() {
+        var flightRequest = FlightRequest.builder()
+                .from(Location.builder().city("Warsaw").country("Poland").build())
+                .build();
+
+        var exception = assertThrows(FlightNotFoundException.class,
+                () ->  flightsFacade.getFlights(flightRequest));
+
+        var expectedMessage = "No flights found for given locations!";
+        var actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void testGetAllConnections() throws FlightNotFoundException {
+
+        var firstCity = "Aberdeen";
+        var firstCountry = "United States";
+        var secondCity = "Minneapolis";
+        var secondCountry = "United States";
+
+        var connectionRequest = ConnectionRequest.builder()
+                .firstLocation(Location.builder().city(firstCity).country(firstCountry).build())
+                .secondLocation(Location.builder().city(secondCity).country(secondCountry).build())
+                .build();
+
+        var foundConnections = flightsFacade.getAllConnections(connectionRequest);
+
+        assertEquals(2, foundConnections.size());
+
+        var firstFoundConnection = foundConnections.get(0);
+
+        assertAll(() -> {
+            assertEquals(671, firstFoundConnection.getNumberOfFlights());
+            assertEquals(firstCity, firstFoundConnection.getFrom().getCity());
+            assertEquals(firstCountry, firstFoundConnection.getFrom().getCountry());
+            assertEquals(secondCity, firstFoundConnection.getTo().getCity());
+            assertEquals(secondCountry, firstFoundConnection.getTo().getCountry());
+        });
+
+        var secondFoundConnection = foundConnections.get(1);
+
+        assertAll(() -> {
+            assertEquals(1340, secondFoundConnection.getNumberOfFlights());
+            assertEquals(secondCity, secondFoundConnection.getFrom().getCity());
+            assertEquals(secondCountry, secondFoundConnection.getFrom().getCountry());
+            assertEquals(firstCity, secondFoundConnection.getTo().getCity());
+            assertEquals(firstCountry, secondFoundConnection.getTo().getCountry());
+        });
+    }
+
+    @Test
+    void testGetAllConnectionsNotFound() {
+        var connectionRequest = ConnectionRequest.builder()
+                .firstLocation(Location.builder().city("Warsaw").country("Poland").build())
+                .secondLocation(Location.builder().city("Krakow").country("Poland").build())
+                .build();
+
+        var exception = assertThrows(FlightNotFoundException.class,
+                () ->  flightsFacade.getAllConnections(connectionRequest));
+
+        var expectedMessage = "No flights found for given locations!";
+        var actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 }
