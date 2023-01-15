8771ef9aee580d3f43b228b9a23e48a063b4c64e
Eryk <emroczko@me.com>
2023-01-15 03:07:22 +0100

    Added some cities tests

--- server/src/test/java/com/tass/flightdiscover/repository/CityRepositoryTests.java
+++ server/src/test/java/com/tass/flightdiscover/repository/CityRepositoryTests.java
@@ -1,7 +1,43 @@
 package com.tass.flightdiscover.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-public class CityRepositoryTests {
+class CityRepositoryTests {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Test
+    void testGetCityByName() {
+        var foundCities = cityRepository.findCityByName("New York");
+
+        assertFalse(foundCities.isEmpty());
+        var foundCity = foundCities.get(0);
+
+        assertAll(() -> {
+            assertEquals("New York", foundCity.getName());
+            assertEquals("United States", foundCity.getCountryName());
+            assertEquals(40.6943, foundCity.getCityLocationY());
+            assertEquals(-73.9249, foundCity.getCityLocationX());
+            assertEquals(19354922L, foundCity.getCityPopulation());
+            assertEquals(183503L, foundCity.getFlightsNumberToCity());
+            assertEquals(183449L, foundCity.getFlightsNumberFromCity());
+            assertEquals(86L, foundCity.getOriginCitiesNumber());
+            assertEquals(84L, foundCity.getDestinationCitiesNumber());
+            assertEquals(86L, foundCity.getTotalConnectedCitiesNumber());
+            assertEquals(2, foundCity.getOriginCountriesNumber());
+            assertEquals(2, foundCity.getDestinationCountriesNumber());
+            assertEquals(3, foundCity.getBusiestMonthFlightsToCity());
+            assertEquals(3, foundCity.getBusiestMonthFlightsFromCity());
+            assertEquals(0.009478157545662029, foundCity.getFlightsNumberFromCityToPopulationRatio());
+            assertEquals(0.009480947533655781, foundCity.getFlightsNumberToCityToPopulationRatio());
+            assertEquals(0.01895910507931781, foundCity.getTotalFlightsNumberForCityToPopulationRatio());
+        });
+    }
+
 }
--- server/src/test/java/com/tass/flightdiscover/service/CityServiceTests.java
+++ server/src/test/java/com/tass/flightdiscover/service/CityServiceTests.java
@@ -1,9 +1,82 @@
 package com.tass.flightdiscover.service;
 
+import com.tass.flightdiscover.domain.city.City;
+import com.tass.flightdiscover.domain.city.CityEntity;
+import com.tass.flightdiscover.repository.CityRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class CityServiceTests {
+class CityServiceTests {
 
+    private static List<City> cities;
+    private static List<CityEntity> cityEntities;
+    @MockBean
+    CityRepository cityRepository;
+    @Autowired
+    CityService cityService;
 
+    @BeforeAll
+    static void prepareData() {
+        cityEntities = List.of(
+                CityEntity.builder()
+                        .name("Warsaw")
+                        .countryName("Poland")
+                        .cityLocationY(1.0)
+                        .cityLocationX(2.0)
+                        .cityPopulation(1700000L)
+                        .destinationCitiesNumber(50L)
+                        .flightsNumberFromCity(100L)
+                        .flightsNumberToCity(200L)
+                        .originCitiesNumber(60L)
+                        .busiestMonthFlightsFromCity(1)
+                        .busiestMonthFlightsToCity(2)
+                        .build());
+
+        cities = List.of(
+                City.builder()
+                        .name("Warsaw")
+                        .countryName("Poland")
+                        .cityLocationY(1.0)
+                        .cityLocationX(2.0)
+                        .cityPopulation(1700000L)
+                        .destinationCitiesNumber(50L)
+                        .flightsNumberFromCity(100L)
+                        .flightsNumberToCity(200L)
+                        .totalFlightsNumber(300L)
+                        .originCitiesNumber(60L)
+                        .busiestMonthFlightsFromCity(1)
+                        .busiestMonthFlightsToCity(2)
+                        .build());
+    }
+
+    @Test
+    void testGetAllCities() {
+
+        Mockito.when(cityRepository.findAll()).thenReturn(cityEntities);
+
+        var foundCities = cityService.getAllCities();
+
+        assertEquals(cities, foundCities);
+    }
+
+    @Test
+    void testGetCityByName() {
+
+        var cityName = "Warsaw";
+
+        Mockito.when(cityRepository.findCityByName(cityName))
+                .thenReturn(cityEntities);
+        var foundCities = cityService.getCityByName(cityName);
+
+        assertEquals(cities, foundCities);
+    }
 }
