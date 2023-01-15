3c2de90a8b7800841495891c47f5f6bf2f55fd2e
Eryk <emroczko@me.com>
2023-01-15 03:06:53 +0100

    Update FlightDiscoverApplicationTests.java

--- server/src/test/java/com/tass/flightdiscover/FlightDiscoverApplicationTests.java
+++ server/src/test/java/com/tass/flightdiscover/FlightDiscoverApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FlightDiscoverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
